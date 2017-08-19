9c669d780e7edcc50ab9de2045913bae72b10ca9
truongvanha <hatv.itedu@gmail.com>
2017-08-19 21:35:29 +0700

    complete splash screen and animation

--- Aplace/app/src/main/java/com/example/ha/aplace/SplashActivity.java
+++ Aplace/app/src/main/java/com/example/ha/aplace/SplashActivity.java
@@ -1,13 +1,55 @@
 package com.example.ha.aplace;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class SplashActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SplashActivity extends AppCompatActivity implements Animation.AnimationListener {
+
+    @BindView(R.id.layoutSplashAct)
+    RelativeLayout layout;
+    @BindView(R.id.imgSplashAct_Logo)
+    ImageView imageLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ButterKnife.bind(this);
+        Animation transitionAnim = AnimationUtils.loadAnimation(this, R.anim.transition_icon);
+        imageLogo.setAnimation(transitionAnim);
+
+        Animation alphaAnim = AnimationUtils.loadAnimation(this, R.anim.alpha_background);
+        layout.setAnimation(alphaAnim);
+        alphaAnim.setAnimationListener(this);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Hello app", Toast.LENGTH_LONG).show();
+            }
+        }, 3000);
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
